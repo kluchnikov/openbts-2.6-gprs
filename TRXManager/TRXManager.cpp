e489cb9d456c8b23c09982c09c56121117c6d6a2
Ivan Kluchnikov <kluchnikovi@gmail.com>
2013-04-18 15:31:13 +0400

    Added channel combinations for GPRS.

--- TRXManager/TRXManager.cpp
+++ TRXManager/TRXManager.cpp
@@ -429,7 +429,7 @@ bool ::ARFCNManager::setTSC(unsigned TSC)
 bool ::ARFCNManager::setSlot(unsigned TN, unsigned combination)
 {
 	assert(TN<8);
-	assert(combination<8);
+	assert(combination<14);
 	char paramBuf[MAX_UDP_LENGTH];
 	sprintf(paramBuf,"%d %d", TN, combination);
 	int status = sendCommand("SETSLOT",paramBuf);
